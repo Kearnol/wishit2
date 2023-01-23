b336fdbaae1c2f185ef162dad8f28322e9250077
Devin Kearns <devin.kearns@outlook.com>
2023-01-22 22:50:36 -0700

    add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
+// Pacakges
 import { useState } from 'react'
+import { Outlet } from 'react-router-dom'
+
+// Components
 import { Helmet } from 'react-helmet'
 import ResponsiveAppBar from './components/common/nav/ResponsiveAppBar'
+
+// Hooks
+// Utils
+// Types
+
 // Material UI fonts
 import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
+// Styles
 import './App.css'
 
 function App() {
@@ -17,6 +27,7 @@ function App() {
                 <title> Wish It Web App</title>
             </Helmet>
             <ResponsiveAppBar />
+            <Outlet />
         </div>
     )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { 
+  createBrowserRouter,
+  RouterProvider,
+ } from "react-router-dom";
 import App from './App'
 import './index.css'
+import ErrorPage from './pages/error-page';
+
+// Set up React-Router
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: 'wishlists',
+        element: <div>Hi! You made it to the wishlists page!!! </div>
+      }
+    ]
+  }
+])
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
