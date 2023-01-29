b190e0dc7aeedb01c14b82118f1ca7cdeaaada50
Devin Kearns <devin.kearns@outlook.com>
2023-01-29 11:18:04 -0700

    implement page routing via routes component

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Auth0Provider } from '@auth0/auth0-react'
+import Routes from './routes/Routes'
 import './index.css'
-import ErrorPage from './pages/error-page';
-
-// Set up React-Router
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: 'wishlists',
-        element: <div>Hi! You made it to the wishlists page!!! </div>
-      }
-    ]
-  }
-])
-
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+    <React.StrictMode>
         <Auth0Provider
             domain="dev-at6uinsfl53etukd.us.auth0.com"
             clientId="XNRFDCgiBELQ9pgWdzka9l8JvscifnjD"
@@ -28,5 +13,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
                 redirect_uri: 'https://localhost:5173/wishlists',
             }}
         >
+            <Routes />
         </Auth0Provider>
+    </React.StrictMode>
 )
