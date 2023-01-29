c595d61ed7b86822767f30f829067062daa3dc89
Devin Kearns <devin.kearns@outlook.com>
2023-01-29 11:15:27 -0700

    implement auth0 login flow

--- src/components/common/nav/ResponsiveAppBar.tsx
+++ src/components/common/nav/ResponsiveAppBar.tsx
@@ -1,18 +1,4 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import { Link } from 'react-router-dom';
-import AutoFixHighOutlinedIcon from '@mui/icons-material/AutoFixHighOutlined';
+import { useAuth0 } from '@auth0/auth0-react'
 
 const pages = ['Wishlists', 'Create', 'Connect', 'Explore'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
@@ -32,9 +18,7 @@ function ResponsiveAppBar() {
     setAnchorElNav(null);
   };
 
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
+    const { user, isAuthenticated, isLoading, logout } = useAuth0()
 
   return (
     <AppBar position="static" sx={{background: "#242426", border: "2px solid white"}}>
@@ -128,38 +112,23 @@ function ResponsiveAppBar() {
             ))}
           </Box>
 
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
-  );
+                                    <Avatar
+                                        alt={user?.name}
+                                        src={
+                                            user?.picture ??
+                                            '/static/images/avatar/2.jpg'
+                                        }
+                                    />
+                                            <MenuItem
+                                                key={setting}
+                                                onClick={() =>
+                                                    logout({
+                                                        logoutParams: {
+                                                            returnTo:
+                                                                'https://localhost:5173/login',
+                                                        },
+                                                    })
+                                                }
+                                            >
 }
 export default ResponsiveAppBar;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { 
-  createBrowserRouter,
-  RouterProvider,
- } from "react-router-dom";
-import App from './App'
+import { Auth0Provider } from '@auth0/auth0-react'
 import './index.css'
 import ErrorPage from './pages/error-page';
 
@@ -25,7 +21,12 @@ const router = createBrowserRouter([
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+        <Auth0Provider
+            domain="dev-at6uinsfl53etukd.us.auth0.com"
+            clientId="XNRFDCgiBELQ9pgWdzka9l8JvscifnjD"
+            authorizationParams={{
+                redirect_uri: 'https://localhost:5173/wishlists',
+            }}
+        >
+        </Auth0Provider>
 )
